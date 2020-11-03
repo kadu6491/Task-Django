f14b829257efc51c8d073e9bd0b4fe89698e914d
kadutwum <kadut001@odu.edu>
2020-11-03 13:51:22 -0500

    made models and template folder

--- Tasks/Todo/admin.py
+++ Tasks/Todo/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_add', 'completed')
+
+
+admin.site.register(Task, TaskAdmin)
+
--- Tasks/Todo/models.py
+++ Tasks/Todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    date_add = models.DateTimeField(default=datetime.now())
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+
