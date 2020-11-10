addf28edabef48949a0a0dbd329be452654e365d
Kofi Adutwum <kadut001@odu.edu>
2020-11-10 18:05:39 -0500

    work

--- Tasks/Tasks/settings.py
+++ Tasks/Tasks/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '*cq_2z!p5oe7!^v(-puu&m8x45#tn#=40!rlx!4-wv8e3hk1t^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kofitask.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
